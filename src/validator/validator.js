6c1cb58305f2d0f755b07914b284d91190539d0c
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-06-18 03:38:59 +0300

    modified validator

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,17 +1,12 @@
 import {Validator} from 'jsonschema'
 
-class QueryValidator {
-  constructor(query, schema) {
+class Validate {
+  query(query, schema) {
     if(!query || !schema) {
       throw 'Query or schema missing!'
     }
-    this.query = query
-    this.schema = schema
-  }
-
-  validate() {
     return new Validator().validate(this.query, this.schema)
   }
 }
 
-export default QueryValidator
+export default Validate
