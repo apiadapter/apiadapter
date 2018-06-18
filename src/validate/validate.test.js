4d5e49687c22f2a7d29f2062a83068e8858a0c6d
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-06-18 04:27:50 +0300

    refactored Validate tests

--- src/validate/validate.test.js
+++ src/validate/validate.test.js
@@ -1,19 +1,21 @@
 import {expect} from 'chai'
 import Validate from './validate.js'
 
-it('Should return a validation error', function(done) {
-  var instance = 'test'
-  var schema = {'type': 'number'}
-  var result = new Validate().query(instance, schema)
-  expect(result.errors).to.have.lengthOf(1)
-  expect(result.errors[0].message).to.equal('is not of a type(s) number')
-  done()
-})
+describe('Validate', function() {
+  describe('.query', function() {
+    it('Should return a validation error', function() {
+      var instance = 'test'
+      var schema = {'type': 'number'}
+      var result = new Validate().query(instance, schema)
+      expect(result.errors).to.have.lengthOf(1)
+      expect(result.errors[0].message).to.equal('is not of a type(s) number')
+    })
 
-it('Should return a ok status', function(done) {
-  var instance = 4
-  var schema = {'type': 'number'}
-  var result = new Validate().query(instance, schema)
-  expect(result.valid).to.equal(true)
-  done()
+    it('Should be valid', function() {
+      var instance = 4
+      var schema = {'type': 'number'}
+      var result = new Validate().query(instance, schema)
+      expect(result.valid).to.equal(true)
+    })
+  })
 })
