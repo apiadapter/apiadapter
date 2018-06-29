b2f3306f8a72b108dc7f61e609d77d5a67fcc450
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-06-30 00:09:20 +0300

    schema validations for querycontroller

--- src/controllers/queryController/get.js
+++ src/controllers/queryController/get.js
@@ -8,15 +8,27 @@ module.exports =  function handler(req, res, next) {
   var querySchema = fs.readFileSync(__dirname + '/../../schemas/query.json', {encoding: 'utf8'})
 
   try {
-    JSON.parse(params.query)
+    var query = JSON.parse(params.query)
     if(!validate.query(JSON.parse(params.query), JSON.parse(querySchema))) {
       res.send(new errors.BadRequestError('Invalid query'))
     } else {
-      res.send(200,'foobar')
+      console.log(query)
+      try {
+        fs.accessSync(__dirname + '/../../schemas/' + query.queryType.toLowerCase() + '.json', fs.F_OK)
+        if(query.includes) {
+          for(var i = 0; i < query.includes.length; i++) {
+            fs.accessSync(__dirname + '/../../schemas/' + query.includes[i].toLowerCase() + '.json', fs.F_OK)
+          }
+        }
+        res.send(200, 'Forward to messagequeue') //TODO break message to messagequeue
+
+      } catch(e) {
+        res.send(new errors.BadRequestError('Querytype not found!' + '\r\n' + e))
+      }
     }
   }
   catch (e) {
-    res.send(new errors.BadRequestError('Invalid query'))
+    res.send(new errors.BadRequestError(e))
   }
   finally {
     next()
