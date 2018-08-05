6033cbc7d0ec683015270ac7ac297e7d6d395c6f
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-08-05 22:21:47 +0300

    querytest changes

--- src/controllers/queryController/get.test.js
+++ src/controllers/queryController/get.test.js
@@ -9,21 +9,49 @@ chai.use(chaiHttp)
 let invalidQuery = fs.readFileSync(__dirname + '/../../../mocks/query_templates/invalid_query.json', {encoding: 'utf8'})
 let validQuery = fs.readFileSync(__dirname + '/../../../mocks/query_templates/valid_query.json', {encoding: 'utf8'})
 
+//Server without authorize
+config.useAuth = false
+let server = new Server(config).test()
+
+describe('QueryController without authorization', () => {
+  describe('.get', () => {
+    it('Should return 400 for invalid query', (done) => {
+      chai.request(server)
+        .get('/query/' + invalidQuery)
+        .end((err, res) => {
+          expect(res).to.have.status(400)
+          server.close()
+          done()
+        })
+    })
+    it('Should return 200 with valid query', (done) => {
+      chai.request(server)
+        .get('/query/' + validQuery)
+        .end((err, res) => {
+          expect(res).to.have.status(200)
+          server.close()
+          done()
+        })
+    })
+  })
+})
+
 //Server with token authorize
 config.useAuth = true
 let authserver = new Server(config).test()
 
 describe('QueryController with authorization', () => {
   describe('.get', () => {
-    it('Should return 401 without token', function () {
+    it('Should return 401 without token', (done) => {
       chai.request(authserver)
         .get('/query/' + validQuery)
         .end((err, res) => {
           expect(res).to.have.status(401)
           server.close()
+          done()
         })
     })
-    it('Should return 200 with token', function () {
+    it('Should return 200 with token', (done) => {
       let apikey = new Apikey()
       chai.request(authserver)
         .get('/query/' + validQuery)
@@ -31,31 +59,7 @@ describe('QueryController with authorization', () => {
         .end((err, res) => {
           expect(res).to.have.status(200)
           server.close()
-        })
-    })
-  })
-})
-
-//Server without authorize
-config.useAuth = false
-let server = new Server(config).test()
-
-describe('QueryController without authorization', () => {
-  describe('.get', () => {
-    it('Should return 400 for invalid query', function () {
-      chai.request(server)
-        .get('/query/' + invalidQuery)
-        .end((err, res) => {
-          expect(res).to.have.status(400)
-          server.close()
-        })
-    })
-    it('Should return 200 with valid query', function () {
-      chai.request(server)
-        .get('/query/' + validQuery)
-        .end((err, res) => {
-          expect(res).to.have.status(200)
-          server.close()
+          done()
         })
     })
   })
